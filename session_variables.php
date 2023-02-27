1f1debf26fd19ff0c829c66a9604617c186fc30c
Adiel Elifelet <71462000+Addy2048@users.noreply.github.com>
2023-02-27 20:50:23 +0300

    final final

---
+++ session_variables.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+echo $_SESSION['alogin'];
+$uname = $_SESSION['alogin'];
+$sql ="SELECT UserName,role FROM admin WHERE StaffId=:uname ";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+
+if($query->rowCount() > 0){
+    foreach($results as $result){
+        $userName = $result->UserName;
+        $role = $result->role;
+    }
+}
+else {
+    echo "Nothing here";
+}
+echo $result->UserName;
+
+?>
